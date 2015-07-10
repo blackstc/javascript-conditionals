0a185cdd1a08f30f136c3dd1711a092f7aad4b49
Blackstock <charlieblackstock@gmail.com>
2015-07-10 16:05:38 -0600

    added postal code prompt

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-var phoneNumber = prompt("Please enter your phone number. (XXX-XXX-XXXX)");
+// var phoneNumber = prompt("Please enter your phone number. (XXX-XXX-XXXX)");
+//
+// if (phoneNumber.indexOf("-") != 3 && phoneNumber.indexOf("-") != 7) {
+//   alert("You have not entered your phone with dashes");
+// }
+//
+// var birthDate = prompt("Please enter your birth date. (xx/xx/xx)");
+//
+// if (birthDate.indexOf("/") != 2 && birthDate.indexOf("/") != 5) {
+//   alert("You have not entered your birth date correctly.");
+// }
 
-if (phoneNumber.indexOf("-") != 3 && phoneNumber.indexOf("-") != 7) {
-  alert("You have not entered your phone with dashes");
-}
+var postalCode = prompt("Please enter your postal code.", "(xxxxx OR xxxxx-xxxx)");
 
-var birthDate = prompt("Please enter your birth date. (xx/xx/xx)");
-
-if (birthDate.indexOf("/") != 2 && birthDate.indexOf("/") != 5) {
-  alert("You have not entered your birth date correctly.");
-}
+if (postalCode.length === 5) {
+  alert("You have correctly entered your postal code!");
+} else if (postalCode.length === 10 && postalCode[5] === "-") {
+  alert("You have correctly entered your postal code!");
+} else {
+  alert("You have not entered your postal code correctly");
+};
