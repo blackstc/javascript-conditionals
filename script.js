9b75f57d3f101aa0aaf2f9dd116794867c062e6f
Blackstock <charlieblackstock@gmail.com>
2015-07-10 16:16:48 -0600

    removed the 'comment-out' from the code.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// var phoneNumber = prompt("Please enter your phone number. (XXX-XXX-XXXX)");
-//
-// if (phoneNumber.indexOf("-") != 3 && phoneNumber.indexOf("-") != 7) {
-//   alert("You have not entered your phone with dashes");
-// }
-//
-// var birthDate = prompt("Please enter your birth date. (xx/xx/xx)");
-//
-// if (birthDate.indexOf("/") != 2 && birthDate.indexOf("/") != 5) {
-//   alert("You have not entered your birth date correctly.");
-// }
+var phoneNumber = prompt("Please enter your phone number. (XXX-XXX-XXXX)");
 
-// var postalCode = prompt("Please enter your postal code.", "(xxxxx OR xxxxx-xxxx)");
-//
-// if (postalCode.length === 5) {
-//   alert("You have correctly entered your postal code!");
-// } else if (postalCode.length === 10 && postalCode[5] === "-") {
-//   alert("You have correctly entered your postal code!");
-// } else {
-//   alert("You have not entered your postal code correctly");
-// };
+if (phoneNumber.indexOf("-") != 3 && phoneNumber.indexOf("-") != 7) {
+  alert("You have not entered your phone with dashes");
+}
+
+var birthDate = prompt("Please enter your birth date. (xx/xx/xx)");
+
+if (birthDate.indexOf("/") != 2 && birthDate.indexOf("/") != 5) {
+  alert("You have not entered your birth date correctly.");
+}
+
+var postalCode = prompt("Please enter your postal code.", "(xxxxx OR xxxxx-xxxx)");
+
+if (postalCode.length === 5) {
+  alert("You have correctly entered your postal code!");
+} else if (postalCode.length === 10 && postalCode[5] === "-") {
+  alert("You have correctly entered your postal code!");
+} else {
+  alert("You have not entered your postal code correctly");
+};
 
 var married = prompt("Are you married", "yes or no")
 var response = married.toUpperCase()
