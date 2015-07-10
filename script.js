950c7eefc41fad257c5930d6c019a3119e04b6cc
Blackstock <charlieblackstock@gmail.com>
2015-07-10 14:47:08 -0600

    added birth date prompt to JS

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ var phoneNumber = prompt("Please enter your phone number. (XXX-XXX-XXXX)");
 if (phoneNumber.indexOf("-") != 3 && phoneNumber.indexOf("-") != 7) {
   alert("You have not entered your phone with dashes");
 }
+
+var birthDate = prompt("Please enter your birth date. (xx/xx/xx)");
+
+if (birthDate.indexOf("/") != 2 && birthDate.indexOf("/") != 5) {
+  alert("You have not entered your birth date correctly.");
+}
